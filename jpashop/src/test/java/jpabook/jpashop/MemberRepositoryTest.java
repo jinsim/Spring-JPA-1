212d2991c3a7b8476bba5fcf3b844a620e8a1f77
jinsim <simsime@korea.ac.kr>
2022-05-08 12:35:10 +0900

    에러 수정 - yml url 변경

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +17,7 @@ public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
     public void testMember() throws Exception {
         //given
         Member member = new Member();
