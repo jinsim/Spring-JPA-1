dcc8562840813a06290348079068a24eb02f530b
jinsim <simsime@korea.ac.kr>
2022-05-13 12:09:06 +0900

    상품 서비스 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    // readOnly = true면 DB에 반영이 되지 않기 때문에 오버라이딩 해서 해당 메소드는 false로 맞춰준다.
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
